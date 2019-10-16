8d99f46f561c9842b654a7e3970e137dde31757d
michel <adolfo.ramirez1939@gmail.com>
2019-10-16 11:20:00 +0200

    partie1 partie2 completed and tested

--- partie1.py
+++ partie1.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 ## 1\.Exercice 1 : Hello World
+print("\nExercise Nr. #1")
+print("---------------")
+
 print("Hello World")
 
 Macron =  "hello world"
@@ -7,6 +10,9 @@ Macron =  "hello world"
 print(Macron)
 
 ## 2\.Exercice 2 : Calculs divers
+print("\nExercise Nr. #2")
+print("---------------")
+
 print(3*3)
 try:
     12/0
@@ -17,15 +23,24 @@ print(12-7)
 print(5e4)
 
 ## 3\.Exercice 3 : Communiquer avec l'ordinateur
+print("\nExercise Nr. #3")
+print("---------------")
+
 name = input("Who should I introduce ? ")
 print("Hi",name,", welcome on board!".format(name))
 
 ## 4\.Exercice 4 : Nom et prénom
+print("\nExercise Nr. #4")
+print("---------------")
+
 name = "Vladimir"
 familyname = "Illitch"
 print("Bonjour {} {}".format(name,familyname))
 
 ## 5\.Exercice 5 : Des caractères au nombre
+print("\nExercise Nr. #5")
+print("---------------")
+
 myNumber = "123"
 print(myNumber)
 print(type(myNumber))
@@ -34,6 +49,9 @@ print(myNumber)
 print(type(myNumber))
 
 ## 6\.Exercice 6 : Majuscules et miniscules
+print("\nExercise Nr. #6")
+print("---------------")
+
 myMAJString = "ALEA JACTA EST"
 print(myMAJString.lower())
 myminString = "alea jacta est"
